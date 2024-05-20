46c13c97d9990c04f8b514d47db81df7b87aca6a
Pradipa Y <merakayamsstraat@gmail.com>
2024-05-20 10:55:56 +0700

    membuat class manusia constructor dan destroctor

--- relation/manusia.h
+++ relation/manusia.h
@@ -3,3 +3,16 @@
 
 using namespace std;
 
+class manusia {
+public:
+	string name;
+	jantung varJantung;
+
+	manusia(string pName) : name(pName) {
+		cout << name << " HIDUP\n ";
+	}
+	~manusia() {
+		cout << name << " MATI\n ";
+	}
+};
+#endif
